a2aee270d273697c59e0302e27f21b524fef392e
Bastien Tran <bastien.tran@comet.team>
2022-11-23 14:08:15 +0100

    initialize rover with position and direction + receive and store array of commands TODO maybe refactor

--- src/rover.spec.ts
+++ src/rover.spec.ts
@@ -1,7 +1,40 @@
-import { Rover } from './rover'
+class Rover {
+    constructor(private x: number, private y: number, private initialDirection: string) {
+
+    }
+    direction: string = this.initialDirection
+
+    position: any = {
+        x: this.x,
+        y: this.y
+    }
+
+    commands: string[] = []
+
+    handleCommands(commands: string[]) {
+        this.commands = commands
+    }   
+}
+
 
 describe('Rover behaviour', () => {
-    it('', () => {
+    it('Should instantiate with intial position and direction for Mars Rover', () => {
+        //Given
+        const marsRover = new Rover(0,0,'N')
+        //When
 
+        //Then
+        expect(marsRover.position.x).toEqual(0);
+        expect(marsRover.position.y).toEqual(0);
+        expect(marsRover.direction).toEqual('N');
+    })
+    it('Should take an array of commands', () => {
+        //Given
+        const commands: string[] = []
+        const marsRover = new Rover(0,0,'N')
+        //When
+        marsRover.handleCommands(commands)
+        //Then
+        expect(marsRover.commands).toEqual(commands);
     })
 })
